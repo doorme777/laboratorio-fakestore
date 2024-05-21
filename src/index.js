79fb3237f0c14d9f761aebd0991e6b72d6ca0fd1
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-05-21 11:18:13 -0600

    🔨 [ADD] Logica del observer, y response de API.
    Primera tarea de la prueba tecnica, lista logica, responser de la API  tambien  vi a ntes la documentación de fakestore,  así supe cómo  poner la paginación, no  lo mencione en el principal pero se supone que el local tiene la posción de donde te quedas talvez esto falle al futuro.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
+let peticion = 5;
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
+const getData = (api) => {
+  fetch(`${api}?offset=${peticion}&limit=10`)
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
+      let output = products.map((product) => {
         // template
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      let newItem = document.createElement("section");
+      newItem.classList.add("Item");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
 const loadData = () => {
   getData(API);
-}
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    // Forma de saber si el scroll llego al final de la página
+    // const positionBottom =
+    //   document.documentElement.scrollTop +
+    //     document.documentElement.clientHeight >=
+    //   document.documentElement.scrollHeight - 15;
+    if (entries[0].isIntersecting) {
+      peticion += 10;
+      getData(API);
+      position();
+    }
+  },
+  {
+    root: $observe,
+    rootMargin: "0px 0px 100% 0px",
+    threshold: 0.15,
+  }
+);
 
 intersectionObserver.observe($observe);
+
+const position = () => {
+  localStorage.setItem("pagination", peticion);
+};
+
+loadData();
