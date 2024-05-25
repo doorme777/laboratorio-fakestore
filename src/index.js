c025b0ad9d02d2946bda39cbe387594d8141e1b1
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-05-25 14:35:23 -0600

    ✅ [UPDATE] Mejora del cuarto punto de la prueba tecnica.
    Ahora la carga de los recurso esta mejor, no voy a mentir me inspire del código de otro.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+// import { SkeletonCard } from "./template/SkeletonCard.mjs"; // Add this line
 const $app = document.getElementById("app");
 const $observe = document.getElementById("observe");
 const API = "https://api.escuelajs.co/api/v1/products";
 const pagination = Number(localStorage.getItem("pagination"));
-let petition = 5;
-// let initialState = true;
+localStorage["pagination"] = 5;
+let initialState = true;
 
-// document.onreadystatechange = () => {
-//   if (document.readyState === "complete") {
-//     hidenSkelleton();
-//   }
+// const loadingSkelleton = () => {
+//   const newItem = document.createElement("section");
+//   newItem.classList.add("Items-Skeleton");
+//   newItem.innerHTML = SkeletonCard();
+//   $app.appendChild(newItem);
+// };
+
+// const hidenSkelleton = () => {
+//   const skeleton = document.querySelector(".Items-Skeleton");
+//   skeleton.style.display = "none";
 // };
 
 window.addEventListener("beforeunload", () => {
@@ -16,54 +23,63 @@ window.addEventListener("beforeunload", () => {
 });
 
 const getData = (api) => {
+  // loadingSkelleton();
   const apiLocal = pagination ?? petition;
-  fetch(`${api}?offset=${apiLocal}&limit=10`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      let products = response;
-      if (products.length === 0) {
-        intersectionObserver.disconnect();
-        let newItem = document.createElement("section");
-        newItem.classList.add("Items");
-        newItem.innerHTML = "<h2>No hay más productos</h2>";
-        $app.appendChild(newItem);
-      } else {
-        console.table(products);
-        let output = products
-          .map((product) => {
-            const { title, price, image } = product;
-            // Agregar una imagen por defecto en caso de que no haya una
-            const imageDefault = "../public/assets/not_found.webp";
-            return `
-            <article class="Card">
-              <img src="${image ?? imageDefault}" alt="${title}"/>
-              <h2>
-                ${title}
-                <small>$ ${price}</small>
-              </h2>
-            </article>`;
-          })
-          .join("");
-        let newItem = document.createElement("section");
-        newItem.classList.add("Items");
-        newItem.innerHTML = output;
-        $app.appendChild(newItem);
-      }
+  try {
+    fetch(api, {
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .catch((error) => console.log(error));
+      .then((response) => response.json())
+      .then((response) => {
+        let products = response;
+        if (products.length === 0) {
+          intersectionObserver.disconnect();
+          let newItem = document.createElement("section");
+          newItem.classList.add("Items");
+          newItem.innerHTML = "<h2>No hay más productos</h2>";
+          $app.appendChild(newItem);
+        } else {
+          let output = products
+            .map((product) => {
+              const { title, price, image } = product;
+              // Agregar una imagen por defecto en caso de que no haya una
+              const imageDefault = "../public/assets/not_found.webp";
+              return `
+              <article class="Card">
+                <img src="${image ?? imageDefault}" alt="${title}"/>
+                <h2>
+                  ${title}
+                  <small>$ ${price}</small>
+                </h2>
+              </article>`;
+            })
+            .join("");
+          let newItem = document.createElement("section");
+          newItem.classList.add("Items");
+          newItem.innerHTML = output;
+          $app.appendChild(newItem);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  } // finally {
+  //   hidenSkelleton();
+  // }
 };
 
 const loadData = async () => {
-  await getData(API);
+  if (initialState) {
+    initialState = false;
+  } else {
+    position();
+  }
+  await getData(`${API}?offset=${localStorage.getItem("pagination")}&limit=10`);
 };
 
 const position = () => {
-  const stringifyPetition = JSON.stringify(petition);
-  localStorage.setItem("pagination", stringifyPetition);
+  localStorage["pagination"] += 10;
   console.log(localStorage.getItem("pagination"));
 };
 
