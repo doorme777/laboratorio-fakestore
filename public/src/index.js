2081d77b5de19e23c88405be9bbe8aa41dbbd4be
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-05-28 18:48:52 -0600

    🔥 [REMOVE] Se quito el try catch en el index.js
    Ahora si hay algun error lo pasa por el catch del directamente .

--- public/src/index.js
+++ public/src/index.js
@@ -18,47 +18,46 @@ window.addEventListener("beforeunload", () => {
 });
 
 const getData = (api) => {
-  try {
-    fetch(api, {
-      headers: {
-        "Content-Type": "application/json",
-      },
+  fetch(api, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      let products = response;
+      if (products.length === 0) {
+        intersectionObserver.disconnect();
+        let newItem = document.createElement("section");
+        newItem.classList.add("Items");
+        newItem.innerHTML = "<h2>No hay más productos</h2>";
+        $app.appendChild(newItem);
+      } else {
+        let output = products
+          .map((product) => {
+            const { title, price, images } = product;
+            // Agregar una imagen por defecto en caso de que no haya una
+            const imageDefault = "../public/assets/not_found.webp";
+            console.log(images);
+            return `
+            <article class="Card">
+              <img src="${images[0]}" alt="${title}"/>
+              <h2>
+                ${title}
+                <small>$ ${price}</small>
+              </h2>
+            </article>`;
+          })
+          .join("");
+        let newItem = document.createElement("section");
+        newItem.classList.add("Items");
+        newItem.innerHTML = output;
+        $app.appendChild(newItem);
+      }
     })
-      .then((response) => response.json())
-      .then((response) => {
-        let products = response;
-        if (products.length === 0) {
-          intersectionObserver.disconnect();
-          let newItem = document.createElement("section");
-          newItem.classList.add("Items");
-          newItem.innerHTML = "<h2>No hay más productos</h2>";
-          $app.appendChild(newItem);
-        } else {
-          let output = products
-            .map((product) => {
-              const { title, price, images } = product;
-              // Agregar una imagen por defecto en caso de que no haya una
-              const imageDefault = "../public/assets/not_found.webp";
-              console.log(images);
-              return `
-              <article class="Card">
-                <img src="${images[0]}" alt="${title}"/>
-                <h2>
-                  ${title}
-                  <small>$ ${price}</small>
-                </h2>
-              </article>`;
-            })
-            .join("");
-          let newItem = document.createElement("section");
-          newItem.classList.add("Items");
-          newItem.innerHTML = output;
-          $app.appendChild(newItem);
-        }
-      });
-  } catch (error) {
-    console.log(error);
-  }
+    .catch((error) => {
+      console.log("Error", error);
+    });
 };
 
 const loadData = async () => {
@@ -67,7 +66,9 @@ const loadData = async () => {
   } else {
     position();
   }
-  await getData(`${API}?offset=${localStorage.getItem("pagination")}&limit=10`);
+  await getData(
+    `${API}?offset=${localStorage.getItem("pagination")}&limit=10hola`
+  );
 };
 
 const position = () => {
